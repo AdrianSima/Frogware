78a3eabc32095e908753763b5a17e188718a9f90
Alexandru Manea <forge161@gmail.com>
2021-09-04 14:50:54 +0200

    added some sprites

---
+++ Environment/AnimationPlayer.gd
@@ -0,0 +1,14 @@
+extends AnimationPlayer
+
+export var 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.get_animation('platform_move')
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
